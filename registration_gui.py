1ea34e27328ffeca4ba5a30c8e63cfdd27a02026
Luvuyo.Tafeni <luvuyotafeni012@gmail.com>
2023-11-16 10:54:18 +0200

    Edited registration gui, added validation, added error handling, password strength

--- registration_gui.py
+++ registration_gui.py
@@ -1,17 +1,28 @@
-# @Author : Thabiso Matsaba
-# @Project : banking-app-py-master
-# @Date:  2023/11/14
-# @Time : 09:28
-
 import tkinter as tk
-from tkinter import ttk, messagebox
-from tkinter import font as tkfont
+import tkinter.font as tkfont
 import random
 import hashlib
+import re
 from database import SessionLocal, User
 from sqlalchemy.exc import IntegrityError
 from login_gui import LoginApp
+from tkinter import ttk, messagebox
 
+def password_strength(password):
+    # Criteria for password strength
+    length_criteria = len(password) >= 8
+    digit_criteria = re.search(r"\d", password) is not None
+    lowercase_criteria = re.search(r"[a-z]", password) is not None
+    uppercase_criteria = re.search(r"[A-Z]", password) is not None
+    special_char_criteria = re.search(r"[!@#$%^&*(),.?\":{}|<>]", password) is not None
+
+    # Check overall strength
+    if length_criteria and digit_criteria and lowercase_criteria and uppercase_criteria and special_char_criteria:
+        return "Strong", "green"
+    elif length_criteria and digit_criteria and lowercase_criteria and uppercase_criteria:
+        return "Medium", "orange"
+    else:
+        return "Weak", "red"
 
 class RegistrationApp:
     def __init__(self, master):
@@ -19,14 +30,11 @@ class RegistrationApp:
         self.master.title("User Registration")
         self.master.geometry("800x800")
 
-        # Use Ubuntu font
         ubuntu_font = tkfont.Font(family="Ubuntu", size=12)
-
-        # Update label and entry styles
         label_style = {'font': ubuntu_font}
         entry_style = {'font': ubuntu_font, 'width': 30}
+        btn_style = {'font': ubuntu_font, 'background': '#3498db', 'foreground': 'white', 'width': 15, 'pady': 5}
 
-        # Labels and entries
         self.label_first_name = tk.Label(master, text="First Name:", **label_style)
         self.label_first_name.pack(pady=10)
 
@@ -42,12 +50,11 @@ class RegistrationApp:
         self.label_gender = tk.Label(master, text="Gender:", **label_style)
         self.label_gender.pack(pady=10)
 
-        # Use a larger font for OptionMenu
         gender_font = tkfont.Font(family="Ubuntu", size=13)
         ttk.Style().configure('TMenubutton', font=gender_font)
 
         self.gender_var = tk.StringVar()
-        self.gender_var.set("Male")  # Default value
+        self.gender_var.set("Male")
         gender_options = ["Male", "Female"]
         self.gender_menu = ttk.OptionMenu(master, self.gender_var, *gender_options)
         self.gender_menu.pack(pady=10)
@@ -64,14 +71,16 @@ class RegistrationApp:
         self.entry_email = tk.Entry(master, **entry_style)
         self.entry_email.pack(pady=10)
 
-        # Password generation checkbox
         self.use_generated_password = tk.BooleanVar()
-        self.use_generated_password.set(False)  # Default value
+        self.use_generated_password.set(False)
 
         self.checkbox_generate_password = tk.Checkbutton(master, text="Generate Strong Password",
                                                          variable=self.use_generated_password,
                                                          command=self.toggle_password_entry, **label_style)
         self.checkbox_generate_password.pack(pady=10)
+
+        self.label_generated_password = tk.Label(master, text="", **label_style)
+        self.label_generated_password.pack(pady=5)
 
         self.label_password = tk.Label(master, text="Password:", **label_style)
         self.label_password.pack(pady=10)
@@ -79,28 +88,58 @@ class RegistrationApp:
         self.entry_password = tk.Entry(master, show="*", **entry_style)
         self.entry_password.pack(pady=10)
 
+        self.show_password_var = tk.BooleanVar()
+        self.show_password_var.set(False)
+
+        self.checkbox_show_password = tk.Checkbutton(master, text="Show Password",
+                                                     variable=self.show_password_var,
+                                                     command=self.toggle_password_visibility, **label_style)
+        self.checkbox_show_password.pack(pady=10)
+
         self.label_password_confirmation = tk.Label(master, text="Confirm Password:", **label_style)
         self.label_password_confirmation.pack(pady=10)
 
         self.entry_password_confirmation = tk.Entry(master, show="*", **entry_style)
         self.entry_password_confirmation.pack(pady=10)
 
-        # Style for the register button
-        btn_style = {'font': ubuntu_font, 'background': '#3498db', 'foreground': 'white', 'width': 15, 'pady': 5}
+        self.label_password_strength = tk.Label(master, text="", **label_style)
+        self.label_password_strength.pack(pady=5)
 
-        # Register button
+        self.entry_phone_number.bind("<FocusOut>", self.validate_phone_number)
+        self.entry_email.bind("<FocusOut>", self.validate_email)
+        self.entry_password.bind("<KeyRelease>", self.update_password_strength)
+
         self.btn_register = tk.Button(master, text="Register", command=self.register_user, **btn_style)
         self.btn_register.pack(pady=10)
 
-    @staticmethod
-    def generate_account_number():
-        return random.randint(100000, 999999)
-
     def toggle_password_entry(self):
-        # Enable or disable the password entry based on the checkbox state
         state = tk.NORMAL if not self.use_generated_password.get() else tk.DISABLED
         self.entry_password.configure(state=state)
         self.entry_password_confirmation.configure(state=state)
+
+        if self.use_generated_password.get():
+            generated_password = self.generate_strong_password()
+            self.label_generated_password.config(text=f"Generated Password: {generated_password}")
+        else:
+            self.label_generated_password.config(text="")
+
+    def toggle_password_visibility(self):
+        show_password = self.show_password_var.get()
+        show_char = "" if show_password else "*"
+        self.entry_password.configure(show=show_char)
+        self.entry_password_confirmation.configure(show=show_char)
+
+    def validate_phone_number(self, event):
+        phone_number = self.entry_phone_number.get()
+        if not (phone_number.isdigit() and len(phone_number) == 10):
+            messagebox.showerror("Error", "Please enter a valid 10-digit phone number.")
+            self.entry_phone_number.delete(0, tk.END)
+
+    def validate_email(self, event):
+        email = self.entry_email.get()
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            messagebox.showerror("Error", "Please enter a valid email address.")
+            self.entry_email.delete(0, tk.END)
 
     def register_user(self):
         first_name = self.entry_first_name.get().strip()
@@ -112,7 +151,6 @@ class RegistrationApp:
 
         if self.use_generated_password.get():
             password = self.generate_strong_password()
-            # Display the generated password in a pop-up dialog
             messagebox.showinfo("Generated Password", f"Your generated password is:\n{password}")
         else:
             password = self.entry_password.get()
@@ -125,8 +163,6 @@ class RegistrationApp:
 
         try:
             account_number = self.generate_account_number()
-
-            # Create a new User instance
             new_user = User(
                 account_number=account_number,
                 first_name=first_name,
@@ -138,7 +174,6 @@ class RegistrationApp:
                 password_confirmation=hashed_password
             )
 
-            # Save the new user to the database
             with SessionLocal() as session:
                 session.add(new_user)
                 session.commit()
@@ -149,10 +184,14 @@ class RegistrationApp:
         except Exception as e:
             messagebox.showerror("Error", f"An error occurred during registration: {e}")
 
-        self.master.destroy()  # Close the registration window
+        self.master.destroy()
         login_window = tk.Tk()
         login_app = LoginApp(login_window)
         login_window.mainloop()
+
+    @staticmethod
+    def generate_account_number():
+        return random.randint(100000, 999999)
 
     @staticmethod
     def generate_strong_password():
@@ -161,6 +200,10 @@ class RegistrationApp:
         generated_password = ''.join(random.choice(characters) for i in range(password_length))
         return generated_password
 
+    def update_password_strength(self, event):
+        password = self.entry_password.get()
+        strength, color = password_strength(password)
+        self.label_password_strength.config(text=f"Password Strength: {strength}", fg=color)
 
 if __name__ == "__main__":
     root = tk.Tk()
